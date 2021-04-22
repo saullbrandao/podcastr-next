2191a2599a0f9a1dfaf9d497a7784fa47a7e3531
Saull Brandão <saull@outlook.com>
2021-04-22 10:54:58 -0300

    Add a slider to the player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,4 +1,11 @@
+import { useContext, useEffect, useRef } from 'react'
+import { PlayerContext } from '../../contexts/PlayerContext'
+import Image from 'next/image'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+
 import styles from './styles.module.scss'
+
 
 export function Player() {
   return (
@@ -16,7 +23,11 @@ export function Player() {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (<Slider
+              trackStyle={{ backgroundColor: '#04d361' }}
+              railStyle={{ backgroundColor: '#9f75ff' }}
+              handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+            />) : (<div className={styles.emptySlider} />)}
           </div>
           <span>00:00</span>
         </div>
