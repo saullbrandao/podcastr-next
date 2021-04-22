f03aec7f5edd884b2e7d5931b9d58baefea4184a
Saull Brandão <saull@outlook.com>
2021-04-22 10:51:06 -0300

    Make the podcast play when the button is clicked

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import ptBR from 'date-fns/locale/pt-BR'
 import { GetStaticProps } from 'next'
 import { api } from '../services/api'
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
+import { useContext } from 'react'
+import { PlayerContext } from '../contexts/PlayerContext'
 import styles from './home.module.scss'
 
 type Episode = {
@@ -24,6 +26,8 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext)
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -47,7 +51,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.publishedAt}</span>
                   <span>{episode.durationAsString}</span>
                 </div>
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
