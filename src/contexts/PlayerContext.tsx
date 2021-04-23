f9fbbdf42926c238ee7b086e601b861888062ba0
Saull Brandão <saull@outlook.com>
2021-04-23 10:37:30 -0300

    Make the shuffle button work

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,10 +13,12 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   isLooping: boolean;
+  isShuffling: boolean;
   play: (episode: Episode) => void;
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
   toggleLoop: () => void;
+  toggleShuffle: () => void;
   playNext: () => void;
   playPrevious: () => void;
   hasNext: boolean;
@@ -35,6 +37,7 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
   const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
 
   function play(episode: Episode) {
     setEpisodeList([episode])
@@ -55,6 +58,9 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   function toggleLoop() {
     setIsLooping(!isLooping)
   }
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
 
   function setPlayingState(state: boolean) {
     setIsPlaying(state)
@@ -64,6 +70,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const hasNext = currentEpisodeIndex < episodeList.length - 1
 
   function playNext() {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    }
     if (hasNext) setCurrentEpisodeIndex(currentEpisodeIndex + 1)
   }
 
@@ -79,8 +89,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         play,
         isPlaying,
         isLooping,
+        isShuffling,
         togglePlay,
         toggleLoop,
+        toggleShuffle,
         setPlayingState,
         playList,
         playNext,
